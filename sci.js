2a493af8275e2f09634c053975f41894832db8d2
shivanipandey5678 <140058874+shivanipandey5678@users.noreply.github.com>
2024-01-29 05:23:17 +0530

    Update sci.js
    updating js

--- sci.js
+++ sci.js
@@ -1,29 +1,7 @@
-const textArray = [
-    "Welcome to my portfolio!",
-    "I'm passionate about...",
-    "Exploring new technologies.",
-    "Building innovative solutions.",
-    "Creating a positive impact.",
-    "Let's connect and collaborate!"
-  ];
-  
-  const typerText = document.getElementById('typer-text');
-  let index = 0;
-  
-  function typeText() {
-    typerText.textContent = textArray[index];
-    index = (index + 1) % textArray.length;
-  }
-  
-  setInterval(typeText, 1000); // Adjust the interval as needed
-
-
-  var typed=new Typed(".text",{
-    Strings:['programmer','frontend developer(React)','full stack developer'],
-    typespeed:400,
-    backspeed:400,
-    backdelay:4000,
-    loop:true,
-   
-
-  });
+var sidemenu=document.getElementById("sidemenu");
+function closemenu(){
+  sidemenu.style.right="-200px";
+}
+function openmenu(){
+  sidemenu.style.right="0";
+}
